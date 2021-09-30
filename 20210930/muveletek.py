5dd6d649cee8511cc6c3b5251cceb4499c1bafd0
Tóth Krisztina <toth.krisztina@sulinet.csmszki>
2021-09-30 14:08:22 +0200

    muveletek

--- 20210930/muveletek.py
+++ 20210930/muveletek.py
@@ -1,7 +1,71 @@
-#kerekítés
+'''#kerekítés
 print(round(3.1415, 3))
 print(round(3.1415, 2))
 #hatványozás
 print(pow(2, 8)) #pow(alap, kitevő)
-#abszolútérték
-#abszol
+#abszolútérték abs(mit)
+print(abs(-12))
+print(abs(12))
+print(min(12,7,9,45,3))
+print(max(12,7,9,45,3))
+#pi
+import math
+a = math.pi
+print(a)
+from math import * #a program elején helyezzük el
+a = pi
+print(a)
+#négyzetgyök -sqrt
+print(sqrt(16))  #math.sqrt()
+#felfelé kerekítés math.ceil(mit kerekítsek)
+print(ceil(3.16))
+#lefelé kerekítés math.floor(mit kerekítsek)
+print(floor(3.56))'''
+#1. a négyzet területe,kerülete
+a = float(input("Add meg a négyzet oldalát! a="))
+k = 4*a
+t = pow(a, 2)
+print("A(z)" ,a, "egység oldalú négyzet kerülete", k "területe pedig", t)
+print("A négyzet területe", t)
+
+
+
+
+#2.a téglalap kerülete, területe
+
+
+
+
+
+
+
+#3.a kör kerülete, területe 2*r*pi   r**2 * pi
+
+
+
+
+
+
+
+#4.a kocka térfogata és felszíne V=a**3 A=6*a**2
+
+
+
+
+
+
+
+
+
+
+#5. derékszögű háromszög 2 befogóját kérd be, add meg az átfogót
+
+
+
+
+
+
+
+
+
+#6.négyzetgyök(a**2+b**2)
