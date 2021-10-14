c468232afd828a05ef56f8b04e2973b4e09fa8f5
Tóth Krisztina <toth.krisztina@sulinet.csmszki>
2021-10-14 09:38:32 +0200

    oraimunka

---
+++ 20211014_ciklusok/ciklusok.py
@@ -0,0 +1,37 @@
+def feladat1():
+    for i in range (1,21):
+        print(i, end=" ")
+    print()
+    #az alatta levő sorban kezdődjön a kiírás
+    for i in range(1,21):
+        print(i)
+#itt kezdődik a főprogram
+
+def feladat2():
+    for i in range(15,93):
+        print(i, end=" ")
+
+def feladat3():
+    for i in range(1,31):
+        if i%2 == 0:
+            print(i, end=" ")
+
+def feladat4():#felső határhely mindkettő egyik egymás mellé másik alá
+    print()
+ def feladat5():
+    print()
+
+
+def feladat10():
+    for i in range(100, -101, -1):
+        if i%9==0:
+            print(i,end=" ")
+
+
+
+
+
+#feladat1()
+#feladat2()
+#feladat3()
+#feladat10()
